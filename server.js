bdfb44643b24b69a4afa591c4188f3dd1625c787
Ryan Colosanti <silentcornball28@Ryans-MacBook-Pro.local>
2019-03-25 10:12:22 -0400

    Set up server and npm vars

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var db = require("./models");
+
+var PORT = 3000;
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });  
